e491f4575d9d15d2172a612475cefefba593ceb0
Alessandro Joabar <joabara@outlook.com>
2021-08-08 22:17:42 -0500

    Update btc-usd.py

--- btc-usd.py
+++ btc-usd.py
@@ -25,12 +25,6 @@ from g_trends import g_trend_pull
 import warnings
 warnings.filterwarnings('ignore')
 
-def get_btc_hist_tms(coin):
-	hist = pd.read_csv("data/" + coin +  "_btc.csv")
-	hist = hist.iloc[::-1]
-	return hist
-
-# Create variables for machine learning model
 def hist_to_model_prep(coin_hist):
 	coin_hist["prev"] = coin_hist["btc_price"].shift(periods=1)
 	coin_hist["btc_price2"] = coin_hist["btc_price"].rolling(2, win_type='triang').mean()
@@ -87,7 +81,7 @@ def model_build_and_run(coin_hist):
 	ft_col = ['btc_price', 'btc_price2', 'btc_price3', 'btc_price4', 'btc_price5', 'btc_price6',
 	'btc_price7', 'btc_price8', 'btc_price9', 'btc_price10', 'btc_price11', 'btc_price12', 'btc_price13',
 	'btc_price14', 'btc_price15', 'btc_price16', 'btc_price17', 'btc_price18', 'btc_price19', 'btc_price20',
-	'btc_price24', 'btc_price72', 'bitcoin']
+	'btc_price24', 'btc_price72', 'ethereum']
 
 	X = coin_hist[ft_col]
 	output = coin_hist[ft_col].tail(1)
@@ -103,7 +97,6 @@ def model_build_and_run(coin_hist):
 		if col_name != "go" or col_name != "go":
 			new_name = "Scale_" + str(col_name)
 			red = red.rename(columns={col_name: new_name})
-
 	red.drop(red.tail(1).index, inplace=True)
 
 	# Go
@@ -115,7 +108,7 @@ def model_build_and_run(coin_hist):
 	p_go = np.array(p_go)
 	o3_go = go_build.predict_proba(y)[[0]][0][0]
 
-	# sell
+	# Sell
 	red = red.drop(columns=['go'])
 	red['sell'] = np.array(coin_hist['sell'])
 	x = red[red.columns[red.columns != 'sell']]
@@ -123,6 +116,7 @@ def model_build_and_run(coin_hist):
 	sell_build = autoML(X_train, X_test, y_train, y_test)
 	p_sell = pd.DataFrame(sell_build.predict_proba(x)).iloc[:,0]
 	p_sell = np.array(p_sell)
+	# print(x.info())
 	o3_sell = sell_build.predict_proba(y)[[0]][0][0]
 
 	pgo_mean = np.mean(p_go)
@@ -149,6 +143,8 @@ def build_models_on_train( model, X_train,  y_train):
     return classifier
 
 def autoML(X_train, X_test, y_train, y_test):
+
+	# Go
 	models_to_run = [LogisticRegression] #, SVR, MLPRegressor, GaussianNB, SGDRegressor]
 
 	max_score = 0
@@ -200,15 +196,15 @@ def hist_to_pnl(coin_hist, start, order_amount):
 	coin_hist["trade_ind"] = pd.to_numeric(coin_hist["go_signal"] | coin_hist["sell_signal"])
 	return coin_hist
 
-index = ['bitcoin']
+index = ['ethereum']
 
 for x in index:
 	import time
 	import datetime
 	time.sleep(1)
 	filename = 'data/' + x + '_btc.csv'
-	coin_price_hist(x, 'usd', 85, 'hourly').to_csv(filename)
-	coin_hist = get_btc_hist_tms(x)
+	coin_price_hist(x, 'btc', 85, 'hourly').to_csv(filename)
+	coin_hist = get_coin_hist_tms(x)
 	cmd_log = pd.DataFrame(columns = ['market_tms', 'btc_price', 'p_go', 'p_sell', 'go_signal', 'sell_signal', 'algo_rt'])
 
 	coin_hist = hist_to_model_prep(coin_hist)
@@ -220,6 +216,7 @@ for x in index:
 	while i < len(coin_hist):
 		x = coin_hist.iloc[i - window : i ,:]
 		y = coin_hist.iloc[i-1:i ,:]
+
 		o = model_build_and_run(x)
 
 		coin_hist["p_go"].iloc[i] =  o['p_go'].values[0]
@@ -230,24 +227,25 @@ for x in index:
 		coin_hist["conviction_sell"].iloc[i] =  o['conviction_sell'].values[0]
 		coin_hist["go_signal"].iloc[i] =  o['go_signal'].values[0]
 		coin_hist["sell_signal"].iloc[i] =  o['sell_signal'].values[0]
+
 		i = i + 1
 		pct = round((i / len(coin_hist))*100, 2)
 		if i % 79 == 0: print("Simulation is " + str(pct) + '% complete')
 
-	coin_hist = hist_to_pnl(coin_hist, 1.5, 0.01)
+	coin_hist = hist_to_pnl(coin_hist, 28, 0.4)
 	coin_hist = coin_hist.iloc[::-1]
-	coin_hist.to_csv(('perf/bitcoin_perf.csv'))
+	coin_hist.to_csv(('perf/ethereum_perf.csv'))
 	cmd = coin_hist[['market_tms', 'btc_price', 'p_go', 'p_sell', 'go_signal', 'sell_signal', 'buy_q', 'sell_q', 'buy_cost', 'sell_rev', 'algo_rt']]
 	std = np.std(coin_hist["btc_price"])
 	n = coin_hist["trade_ind"].sum()
 	cmd['ntrades'] = n
 	feed = cmd.head(1).iloc[0]
 	print("----------------------------------------------------------------------------------------------")
-	print(index)
+	print(x)
 	print("----------------------------------------------------------------------------------------------")
 	print(feed)
 	print(str(n))
-  # Uncomment below to push orders to Coinbase Account
+	# Uncomment below to push order to coinbase account
 	# push_order(feed)
 	get_fills_hist('ETH-BTC')
 	print("----------------------------------------------------------------------------------------------")
