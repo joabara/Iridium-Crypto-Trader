bc1dd3966a62faa39dbfde2c22582b603294b109
Alessandro Joabar <joabara@outlook.com>
2021-08-26 19:50:16 -0500

    Update Trader.py

--- Trader.py
+++ Trader.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 class Trader(object):
 	def __init__(self, name, coin, coin2, cbp_market):
 		# super(ClassName, self).__init__()
@@ -39,30 +38,30 @@ class Trader(object):
 		self.hist = hist.iloc[::-1]
 
 		coin_hist = self.hist
-		coin_hist["prev"] = coin_hist["btc_price"].shift(periods=1)
-		coin_hist["btc_price2"] = coin_hist["btc_price"].rolling(2, win_type='triang').mean()
-		coin_hist["btc_price3"] = coin_hist["btc_price"].rolling(3, win_type='triang').mean()
-		coin_hist["btc_price4"] = coin_hist["btc_price"].rolling(4, win_type='triang').mean()
-		coin_hist["btc_price5"] = coin_hist["btc_price"].rolling(5, win_type='triang').mean()
-		coin_hist["btc_price6"] = coin_hist["btc_price"].rolling(6, win_type='triang').mean()
-		coin_hist["btc_price7"] = coin_hist["btc_price"].rolling(7, win_type='triang').mean()
-		coin_hist["btc_price8"] = coin_hist["btc_price"].rolling(8, win_type='triang').mean()
-		coin_hist["btc_price9"] = coin_hist["btc_price"].rolling(9, win_type='triang').mean()
-		coin_hist["btc_price10"] = coin_hist["btc_price"].rolling(10, win_type='triang').mean()
-		coin_hist["btc_price11"] = coin_hist["btc_price"].rolling(11, win_type='triang').mean()
-		coin_hist["btc_price12"] = coin_hist["btc_price"].rolling(12, win_type='triang').mean()
-		coin_hist["btc_price13"] = coin_hist["btc_price"].rolling(13, win_type='triang').mean()
-		coin_hist["btc_price14"] = coin_hist["btc_price"].rolling(14, win_type='triang').mean()
-		coin_hist["btc_price15"] = coin_hist["btc_price"].rolling(15, win_type='triang').mean()
-		coin_hist["btc_price16"] = coin_hist["btc_price"].rolling(16, win_type='triang').mean()
-		coin_hist["btc_price17"] = coin_hist["btc_price"].rolling(17, win_type='triang').mean()
-		coin_hist["btc_price18"] = coin_hist["btc_price"].rolling(18, win_type='triang').mean()
-		coin_hist["btc_price19"] = coin_hist["btc_price"].rolling(19, win_type='triang').mean()
-		coin_hist["btc_price20"] = coin_hist["btc_price"].rolling(20, win_type='triang').mean()
-		coin_hist["btc_price24"] = coin_hist["btc_price"].rolling(24, win_type='triang').mean()
-		coin_hist["btc_price72"] = coin_hist["btc_price"].rolling(72, win_type='triang').mean()
-		coin_hist["next"] = coin_hist["btc_price12"].shift(periods=-24)
-		coin_hist["n_bp"] = (coin_hist["next"]/coin_hist["btc_price"] - 1)
+		coin_hist["prev"] = coin_hist["usd_price"].shift(periods=1)
+		coin_hist["usd_price2"] = coin_hist["usd_price"].rolling(2, win_type='triang').mean()
+		coin_hist["usd_price3"] = coin_hist["usd_price"].rolling(3, win_type='triang').mean()
+		coin_hist["usd_price4"] = coin_hist["usd_price"].rolling(4, win_type='triang').mean()
+		coin_hist["usd_price5"] = coin_hist["usd_price"].rolling(5, win_type='triang').mean()
+		coin_hist["usd_price6"] = coin_hist["usd_price"].rolling(6, win_type='triang').mean()
+		coin_hist["usd_price7"] = coin_hist["usd_price"].rolling(7, win_type='triang').mean()
+		coin_hist["usd_price8"] = coin_hist["usd_price"].rolling(8, win_type='triang').mean()
+		coin_hist["usd_price9"] = coin_hist["usd_price"].rolling(9, win_type='triang').mean()
+		coin_hist["usd_price10"] = coin_hist["usd_price"].rolling(10, win_type='triang').mean()
+		coin_hist["usd_price11"] = coin_hist["usd_price"].rolling(11, win_type='triang').mean()
+		coin_hist["usd_price12"] = coin_hist["usd_price"].rolling(12, win_type='triang').mean()
+		coin_hist["usd_price13"] = coin_hist["usd_price"].rolling(13, win_type='triang').mean()
+		coin_hist["usd_price14"] = coin_hist["usd_price"].rolling(14, win_type='triang').mean()
+		coin_hist["usd_price15"] = coin_hist["usd_price"].rolling(15, win_type='triang').mean()
+		coin_hist["usd_price16"] = coin_hist["usd_price"].rolling(16, win_type='triang').mean()
+		coin_hist["usd_price17"] = coin_hist["usd_price"].rolling(17, win_type='triang').mean()
+		coin_hist["usd_price18"] = coin_hist["usd_price"].rolling(18, win_type='triang').mean()
+		coin_hist["usd_price19"] = coin_hist["usd_price"].rolling(19, win_type='triang').mean()
+		coin_hist["usd_price20"] = coin_hist["usd_price"].rolling(20, win_type='triang').mean()
+		coin_hist["usd_price24"] = coin_hist["usd_price"].rolling(24, win_type='triang').mean()
+		coin_hist["usd_price72"] = coin_hist["usd_price"].rolling(72, win_type='triang').mean()
+		coin_hist["next"] = coin_hist["usd_price12"].shift(periods=-24)
+		coin_hist["n_bp"] = (coin_hist["next"]/coin_hist["usd_price"] - 1)
 		coin_hist["sell"] = coin_hist["n_bp"] > np.mean(coin_hist["n_bp"]) + 1*np.std(coin_hist["n_bp"])
 		coin_hist["sell"] = coin_hist["sell"].astype('int32')
 		coin_hist["go"] = coin_hist["n_bp"] < np.mean(coin_hist["n_bp"]) - 1*np.std(coin_hist["n_bp"])
@@ -89,8 +88,6 @@ class Trader(object):
 
 		self.coin_hist = coin_hist
 
-
-
 	def learn_and_sim(self, window):
 		i = window
 		coin_hist = self.coin_hist
@@ -116,6 +113,26 @@ class Trader(object):
 
 		self.coin_hist = coin_hist
 
+	def learn(self, window):
+		i = window
+		coin_hist = self.coin_hist
+		daysago = len(coin_hist)-window-1
+		yesterday = len(coin_hist)-1
+		today = len(coin_hist)-1
+
+		x = coin_hist.iloc[daysago : yesterday ,:]
+		y = coin_hist.iloc[today ,:]
+		o = self.model_build_and_run(x)
+		output = o[2]
+		self.go_build = o[0]
+		self.sell_build = o[1]
+
+		d =  [output['p_go'].values[0], output['p_sell'].values[0], output['o3_go'].values[0], 
+		output['o3_sell'].values[0], output['conviction_go'].values[0], output['conviction_sell'].values[0], 
+		output['go_signal'].values[0], output['sell_signal'].values[0]]
+
+		self.decision =  d
+
 
 	def model_build_and_run(self, coin_hist):
 		from sklearn.preprocessing import StandardScaler
@@ -123,10 +140,10 @@ class Trader(object):
 		import pandas as pd
 		import numpy as np
 		# coin_hist = self.coin_hist
-		ft_col = ['btc_price', 'btc_price2', 'btc_price3', 'btc_price4', 'btc_price5', 'btc_price6',
-		'btc_price7', 'btc_price8', 'btc_price9', 'btc_price10', 'btc_price11', 'btc_price12', 'btc_price13',
-		'btc_price14', 'btc_price15', 'btc_price16', 'btc_price17', 'btc_price18', 'btc_price19', 'btc_price20',
-		'btc_price24', 'btc_price72', self.coin]
+		ft_col = ['usd_price', 'usd_price2', 'usd_price3', 'usd_price4', 'usd_price5', 'usd_price6',
+		'usd_price7', 'usd_price8', 'usd_price9', 'usd_price10', 'usd_price11', 'usd_price12', 'usd_price13',
+		'usd_price14', 'usd_price15', 'usd_price16', 'usd_price17', 'usd_price18', 'usd_price19', 'usd_price20',
+		'usd_price24', 'usd_price72', self.coin]
 
 		X = coin_hist[ft_col]
 		output = coin_hist[ft_col].tail(1)
@@ -151,7 +168,8 @@ class Trader(object):
 		go_build = self.autoMachineLearning(X_train, X_test, y_train, y_test)
 		p_go = pd.DataFrame(go_build.predict_proba(x)).iloc[:,0]
 		p_go = np.array(p_go)
-		o3_go = go_build.predict_proba(y)[[0]][0][0]
+		o3_go = pd.DataFrame(go_build.predict_proba(y)).iloc[:,0]
+		o3_go = np.array(o3_go)
 
 		# Sell
 		red = red.drop(columns=['go'])
@@ -161,7 +179,8 @@ class Trader(object):
 		sell_build = self.autoMachineLearning(X_train, X_test, y_train, y_test)
 		p_sell = pd.DataFrame(sell_build.predict_proba(x)).iloc[:,0]
 		p_sell = np.array(p_sell)
-		o3_sell = sell_build.predict_proba(y)[[0]][0][0]
+		o3_sell = pd.DataFrame(sell_build.predict_proba(y)).iloc[:,0]
+		o3_sell = np.array(o3_sell)
 
 		pgo_mean = np.mean(p_go)
 		pgo_sd = np.std(p_go)
@@ -172,36 +191,46 @@ class Trader(object):
 		output["p_sell"] = psell_mean
 		output["o3_go"] = o3_go
 		output["o3_sell"] = o3_sell
-		output["conviction_go"] = abs(o3_go/pgo_sd)/10
-		output["conviction_sell"] = abs(o3_sell/psell_sd)/10
-		output["go_signal"] =  int(o3_go - o3_sell > 0.12)
-		output["sell_signal"] = int(o3_sell - o3_go > 0.12)
+		output["conviction_go"] = abs(o3_go)
+		output["conviction_sell"] = abs(o3_sell)
+		output["go_signal"] =  int(o3_go - o3_sell > 0.10)
+		output["sell_signal"] = int(o3_sell - o3_go > 0.10)
 
 		return go_build, sell_build, output
 
 	def autoMachineLearning(self, X_train, X_test, y_train, y_test):
 		from sklearn.linear_model import LogisticRegression
-		# from sklearn.tree import DecisionTreeRegressor
-		# from sklearn.ensemble import RandomForestRegressor
-		# from sklearn.naive_bayes import GaussianNB
-		# from sklearn.linear_model import Perceptron
-		# from sklearn.neural_network import MLPRegressor
-		models_to_run = [LogisticRegression] #, SVR, MLPRegressor, GaussianNB, SGDRegressor]
+		from sklearn.svm import SVC, LinearSVC
+		from sklearn.neighbors import KNeighborsClassifier
+		from sklearn.tree import DecisionTreeClassifier
+		from sklearn.ensemble import RandomForestClassifier
+		from sklearn.naive_bayes import GaussianNB
+		from sklearn.linear_model import Perceptron
+		from sklearn.neural_network import MLPClassifier
+		from sklearn.linear_model import SGDClassifier
+		from sklearn.ensemble import GradientBoostingClassifier
+		from sklearn.model_selection import train_test_split
+		from sklearn import metrics
+		import numpy as np
+
+		models_to_run = [DecisionTreeClassifier] #, GradientBoostingClassifier, SGDClassifier] #, SVR, MLPRegressor, GaussianNB, SGDRegressor, LogisticRegression, LinearSVC, ]
 		max_score = 0
 		max_build = 0
-
+		max_RMSE = 9999
 		for algo in models_to_run:
 			build = self.build_models_on_train(algo, X_train, y_train)
 			pred = build.predict(X_test)
+			RMSE = np.sqrt(metrics.mean_squared_error(y_test, pred))
 			# print("MAE = {:5.4f}".format(metrics.mean_absolute_error(y_test, pred)))
 			# print("MSE = {:5.4f}".format(metrics.mean_squared_error(y_test, pred)))
 			# print("RMSE = {:5.4f}".format(np.sqrt(metrics.mean_squared_error(y_test, pred))))
 			# print("Score:", build.score(X_test, y_test))
 			# print()
-
-			if build.score(X_test, y_test) > max_score:
+			if RMSE < max_RMSE:
+			# if build.score(X_test, y_test) > max_score:
 				max_score = build.score(X_test, y_test)
 				max_build = build
+				max_RMSE = RMSE
 
 		predicted = max_build.predict(X_test)
 
@@ -209,7 +238,7 @@ class Trader(object):
 		# print("RESULTS: ")
 		# print("---------------------------------------------------------------------------------")
 		# print("Best build model is: ")
-		# print(max_build)
+		# print(str(max_build) + "   RMSE: " + str(max_RMSE) + "   Score: " + str(max_score))
 		# print("Build model score: " + str(max_score))
 		# print("MAE = {:5.4f}".format(metrics.mean_absolute_error(y_test, predicted)))
 		# print("MSE = {:5.4f}".format(metrics.mean_squared_error(y_test, predicted)))
@@ -230,13 +259,13 @@ class Trader(object):
 		coin_hist["buy_q"] = coin_hist["go_signal"] * coin_hist["conviction_go"] * order_amount
 		coin_hist["sell_q"] = coin_hist["sell_signal"] * coin_hist["conviction_sell"] * order_amount
 		coin_hist["q"] = start + (coin_hist["buy_q"].cumsum() - coin_hist["sell_q"].cumsum())
-		coin_hist["sell_rev"] = coin_hist["sell_q"] * coin_hist["sell_signal"] * coin_hist["btc_price"]
-		coin_hist["buy_cost"] = coin_hist["buy_q"] * coin_hist["go_signal"] * coin_hist["btc_price"]
+		coin_hist["sell_rev"] = coin_hist["sell_q"] * coin_hist["sell_signal"] * coin_hist["usd_price"]
+		coin_hist["buy_cost"] = coin_hist["buy_q"] * coin_hist["go_signal"] * coin_hist["usd_price"]
 		coin_hist["TRev"] = coin_hist["sell_rev"].cumsum()
 		coin_hist["TCost"] = coin_hist["buy_cost"].cumsum()
-		coin_hist["curr_val"] = coin_hist["q"] * coin_hist["btc_price"]
+		coin_hist["curr_val"] = coin_hist["q"] * coin_hist["usd_price"]
 		coin_hist["cash_flow"] = coin_hist["TRev"] - coin_hist["TCost"]
-		coin_hist["hold_val"] = start * coin_hist["btc_price"]
+		coin_hist["hold_val"] = start * coin_hist["usd_price"]
 		coin_hist["pnl"] = (coin_hist["curr_val"] + coin_hist["cash_flow"]) - coin_hist["hold_val"]
 		coin_hist["algo_rt"] = (coin_hist["curr_val"] + coin_hist["cash_flow"])/coin_hist["hold_val"]
 		coin_hist["trade_ind"] = pd.to_numeric(coin_hist["go_signal"] | coin_hist["sell_signal"])
@@ -246,8 +275,8 @@ class Trader(object):
 		import numpy as np
 		coin_hist = self.coin_hist.iloc[::-1]
 		coin_hist.to_csv(('perf/bitcoin_perf.csv'))
-		cmd = coin_hist[['market_tms', 'btc_price', 'p_go', 'p_sell', 'go_signal', 'sell_signal', 'buy_q', 'sell_q', 'buy_cost', 'sell_rev', 'algo_rt']]
-		std = np.std(coin_hist["btc_price"])
+		cmd = coin_hist[['market_tms', 'usd_price', 'p_go', 'p_sell', 'go_signal', 'sell_signal', 'buy_q', 'sell_q', 'buy_cost', 'sell_rev', 'algo_rt']]
+		std = np.std(coin_hist["usd_price"])
 		n = coin_hist["trade_ind"].sum()
 		cmd['ntrades'] = n
 		feed = cmd.head(1).iloc[0]
@@ -260,38 +289,26 @@ class Trader(object):
 		# get_fills_hist('BTC-USD')
 		print("----------------------------------------------------------------------------------------------")
 		print("")
-	
 
-# test_bot = Trader('Iridium-01', 'bitcoin', 'usd', 'BTC-USD')
-# test_bot.learn_and_sim(21*24)
-# test_bot.hist_to_pnl(1.5, 0.015)
-# test_bot.summary()
+	def push_order(feed):
+		buy_cost = round(float(feed[8]),4)
+		sell_rev = round(float(feed[9]),4)
 
-# hive = []
-# index = pd.read_csv('data/00_coinlist.csv')['coin_id']
-# # index = ['bitcoin', 'ethereum', 'dogecoin', 'cardano', 'cosmos']
-# i = 0
-# for x in index:
-# 	try: hive.append(Trader('Iridium-00'+str(i), x, 'usd', 'Sim'))
-# 	except KeyError: print(x + " doesn't work...")
-# 	i = i + 1
-#
-# for bot in hive:
-# 	bot.learn_and_sim(28*24)
-# 	bot.hist_to_pnl(1.0, 0.02)
-# 	bot.summary()
-#
-#
-# df = pd.DataFrame()
-#
-# for bot in hive:
-# 	df[bot.coin] = bot.coin_hist['algo_rt']
-# # 	x = bot.coin_hist['algo_rt'].tail(1).iloc[0]
-# # 	returns.append((x))
-# # 	names.append(bot.coin)
-#
-# import numpy as np
-# safest = 3
-#
-# for x in df:
-# 	if np.std(x) < safest: safest = np.std(x)
+		if (buy_cost > 0) or (sell_rev > 0):
+			import cbpro
+			from authentication import (api_secret, api_key, api_pass)
+			from datetime import datetime
+			now = datetime.now()
+			url = 'https://api-public.sandbox.pro.coinbase.com'
+			client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass, api_url=url)
+			order_transcript = "No Action"
+
+			if( buy_cost > 0):
+				order_transcript = str( client.place_market_order(product_id='ETH-BTC', side= 'buy', funds = buy_cost))
+
+			if (sell_rev > 0):
+				order_transcript = str(client.place_market_order(product_id='ETH-BTC', side= 'sell', funds = sell_rev))
+
+			log = open("eth-btc-log.txt", "a")
+			n = log.write((str(now) + " | " + "Buy: " + str(buy_cost) + "Sell: " + str(sell_rev) + " " + order_transcript + "\n"))
+			log.close()
